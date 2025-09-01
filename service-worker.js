c7c7787d12c148c794d5de5855406f44bb7cf4a8
magrethkakwi2-creator <magrethkakwi2@gmail.com>
2025-09-01 09:57:13 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("diary-store").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
